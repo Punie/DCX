d45e6cf4df32e9374d375b946b7f107e2a0490e5
Hugo Saracino <hugo.saracino@gmail.com>
2016-10-19 14:56:47 +0200

    Files are the worst !

--- 04_Files/src/fr/formation/files/Main.java
+++ 04_Files/src/fr/formation/files/Main.java
@@ -1,6 +1,7 @@
 package fr.formation.files;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,45 +9,37 @@ import java.util.List;
 
 public class Main
 {
-	public static void printFileShenanigans (String file)
+	public static void printFileShenanigans (Path p) throws IOException
 	{
-		Path p = Paths.get (file);
-		try
-		{
-			List<String> lines = Files.readAllLines (p);
-			
-			// Print Vehicle.java in upper case
-			System.out.println ("UPPER CASE\n----------\n");
-			lines.forEach (line -> System.out.println (line.toUpperCase ()));
-			
-			System.out.println ("\n\n");
-			
-			// Print Vehicle.java from the bottom up
-			System.out.println ("BOTTOM UP\n----------\n");
-			for (int i = lines.size () - 1 ; i >= 0 ; --i)
-				System.out.println (lines.get (i));
-			
-			System.out.println ("\n\n");
-			
-			// Print Vehicle.java with each line inverted
-			System.out.println ("MIRROR\n----------\n");
-			for(String line : lines)
-            	System.out.println (new StringBuilder (line).reverse ());
-			
-			System.out.println ("\n\n");
-			
-			// Print Vehicle.java in full reverse and upper case
-			System.out.println ("REVERSE UPPER CASE\n----------\n");
-			for(int i = lines.size () - 1 ; i >= 0 ; --i)
-				System.out.println (new StringBuilder (lines.get (i)).reverse ().toString ().toUpperCase ());
-		}
-		catch (IOException e)
-		{
-			System.out.println ("Error : " + e.getMessage () + " could not be opened!\n");
-		}
+		List<String> lines = Files.readAllLines (p);
+		
+		// Print Vehicle.java in upper case
+		System.out.println ("UPPER CASE\n----------\n");
+		lines.forEach (line -> System.out.println (line.toUpperCase ()));
+		
+		System.out.println ("\n\n");
+		
+		// Print Vehicle.java from the bottom up
+		System.out.println ("BOTTOM UP\n----------\n");
+		for (int i = lines.size () - 1 ; i >= 0 ; --i)
+			System.out.println (lines.get (i));
+		
+		System.out.println ("\n\n");
+		
+		// Print Vehicle.java with each line inverted
+		System.out.println ("MIRROR\n----------\n");
+		for(String line : lines)
+           	System.out.println (new StringBuilder (line).reverse ());
+		
+		System.out.println ("\n\n");
+		
+		// Print Vehicle.java in full reverse and upper case
+		System.out.println ("REVERSE UPPER CASE\n----------\n");
+		for(int i = lines.size () - 1 ; i >= 0 ; --i)
+			System.out.println (new StringBuilder (lines.get (i)).reverse ().toString ().toUpperCase ());
 	}
 	
-	public static void testFiles ()
+	public static void testFiles () throws IOException
 	{
 		Path p1 = Paths.get ("src/fr/formation/files/Main.java");
 		Path p2 = Paths.get ("src/");
@@ -73,68 +66,76 @@ public class Main
 		System.out.println ("p4 isRegularFile? " + Files.isRegularFile (p4));
 		System.out.println ();
 		
-		try
-		{
-			System.out.println ("p1 isHidden? " + Files.isHidden (p1));
-			System.out.println ("p2 isHidden? " + Files.isHidden (p2));
-			System.out.println ("p4 isHidden? " + Files.isHidden (p4));
-			System.out.println ();
-		}		
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
+		System.out.println ("p1 isHidden? " + Files.isHidden (p1));
+		System.out.println ("p2 isHidden? " + Files.isHidden (p2));
+		System.out.println ("p4 isHidden? " + Files.isHidden (p4));
+		System.out.println ();
 		
-		try
-		{
-			System.out.println ("p1 isSameFile as p3? " + Files.isSameFile (p1, p3));
-			System.out.println ("p1 isSameFile as p4? " + Files.isSameFile (p1, p4));
-			System.out.println ();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
+		System.out.println ("p1 isSameFile as p3? " + Files.isSameFile (p1, p3));
+		System.out.println ("p1 isSameFile as p4? " + Files.isSameFile (p1, p4));
+		System.out.println ();
 		
 		System.out.println ("p1 isWritable? " + Files.isWritable (p1));
 		System.out.println ("p2 isWritable? " + Files.isWritable (p2));
 		System.out.println ("p4 isWritable? " + Files.isWritable (p4));
 	}
 	
-	public static void copyFile (String file1, String file2)
+	public static void copyFile (Path p1, Path p2) throws IOException
 	{
-		Path p1 = Paths.get (file1);
-		Path p2 = Paths.get (file2);
-		try
+		Files.copy (p1, p2);
+	}
+	
+	public static void deleteFile (Path p) throws IOException
+	{
+		boolean exists = Files.deleteIfExists (p);
+		System.out.println (exists);
+	}
+	
+	public static void ls (Path p) throws IOException
+	{
+		DirectoryStream<Path> ds = Files.newDirectoryStream (p);
+		for (Path path : ds)
 		{
-			Files.copy (p1, p2);
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
+			System.out.println (" --> " + path);
+			if (Files.isDirectory (path))
+			{
+				ls (path);
+			}
 		}
 	}
 	
-	public static void deleteFile (String file)
+	public static void tree (Path p, String pre) throws IOException
 	{
-		Path p = Paths.get (file);
-		try
+		DirectoryStream<Path> ds = Files.newDirectoryStream (p);
+		for (Path path : ds)
 		{
-			boolean exists = Files.deleteIfExists (p);
-			System.out.println (exists);
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
+			System.out.println (pre + " --> " + path.getFileName ());
+			if (Files.isDirectory (path))
+			{
+				tree (path, pre + "   ");
+			}
 		}
 	}
 	
 	
 	public static void main (String[] args)
 	{
-		printFileShenanigans("../03_Exceptions/src/fr/formation/exceptions/Vehicle.java");
-		testFiles();
-		copyFile ("../03_Exceptions/src/fr/formation/exceptions/Vehicle.java", "../../../Desktop/Vehicle.txt");
-		deleteFile("../../../Desktop/Vehicle.txt");	
+		Path pFile = Paths.get ("../03_Exceptions/src/fr/formation/exceptions/Vehicle.java");
+		Path pCopy = Paths.get ("../../../Desktop/Vehicle.txt");
+		Path pDir = Paths.get ("../00_Wiki");
+
+		try
+		{
+			printFileShenanigans(pFile);
+			testFiles();
+			copyFile (pFile, pCopy);
+			deleteFile(pCopy);
+			tree (pDir, "");
+			ls (pDir);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
