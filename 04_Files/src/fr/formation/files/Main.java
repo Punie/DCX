9e8e9ce37e1be3f5fca18fc28425ae3ff901ce1f
Hugo Saracino <hugo.saracino@gmail.com>
2016-10-19 12:27:35 +0200

    Files T_T

--- 04_Files/src/fr/formation/files/Main.java
+++ 04_Files/src/fr/formation/files/Main.java
@@ -10,7 +10,7 @@ public class Main
 {
 	public static void main (String[] args)
 	{
-		Path p = Paths.get ("../03_Exceptions/src/fr/formation/exceptions/Vehicle.java");
+		Path p = Paths.get ("../03Exceptions/src/fr/formation/exceptions/Vehicle.java");
 		try
 		{
 			List<String> lines = Files.readAllLines (p);
@@ -42,7 +42,59 @@ public class Main
 		}
 		catch (IOException e)
 		{
-			System.out.println ("Error : " + e.getMessage () + " could not be opened!");
+			System.out.println ("Error : " + e.getMessage () + " could not be opened!\n");
 		}
+		
+		Path p1 = Paths.get ("src/fr/formation/files/Main.java");
+		Path p2 = Paths.get ("src/");
+		Path p3 = Paths.get ("src/fr/formation/files/Main.java");
+		Path p4 = Paths.get ("../.gitignore");
+		
+		System.out.println ("p1 isDirectory? " + Files.isDirectory (p1));
+		System.out.println ("p2 isDirectory? " + Files.isDirectory (p2));
+		System.out.println ("p4 isDirectory? " + Files.isDirectory (p4));
+		System.out.println ();
+		
+		System.out.println ("p1 isExecutable? " + Files.isExecutable (p1));
+		System.out.println ("p2 isExecutable? " + Files.isExecutable (p2));
+		System.out.println ("p4 isExecutable? " + Files.isExecutable (p4));
+		System.out.println ();
+				
+		System.out.println ("p1 isReadable? " + Files.isReadable (p1));
+		System.out.println ("p2 isReadable? " + Files.isReadable (p2));
+		System.out.println ("p4 isReadable? " + Files.isReadable (p4));
+		System.out.println ();
+		
+		System.out.println ("p1 isRegularFile? " + Files.isRegularFile (p1));
+		System.out.println ("p2 isRegularFile? " + Files.isRegularFile (p2));
+		System.out.println ("p4 isRegularFile? " + Files.isRegularFile (p4));
+		System.out.println ();
+		
+		try
+		{
+			System.out.println ("p1 isHidden? " + Files.isHidden (p1));
+			System.out.println ("p2 isHidden? " + Files.isHidden (p2));
+			System.out.println ("p4 isHidden? " + Files.isHidden (p4));
+			System.out.println ();
+		}		
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		try
+		{
+			System.out.println ("p1 isSameFile as p3? " + Files.isSameFile (p1, p3));
+			System.out.println ("p1 isSameFile as p4? " + Files.isSameFile (p1, p4));
+			System.out.println ();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		System.out.println ("p1 isWritable? " + Files.isWritable (p1));
+		System.out.println ("p2 isWritable? " + Files.isWritable (p2));
+		System.out.println ("p4 isWritable? " + Files.isWritable (p4));
 	}
 }
--- 05_Kata/src/fr/formation/kata/FizzBuzz.java
+++ 05_Kata/src/fr/formation/kata/FizzBuzz.java
@@ -22,7 +22,7 @@ public class FizzBuzz
 	
 	public static void main (String[] args)
 	{
-		for (int i = 1 ; i <= 30 ; ++i)
+		for (int i = 1 ; i <= 100 ; ++i)
 			print (i);
 	}
 }
