4b5f2662eecaff054265cc18a530527071feaa07
Hugo Saracino <hugo.saracino@gmail.com>
2016-10-21 17:28:23 +0200

    JDBC killed me

--- 07_JDBC/src/fr/formation/database/MainApp.java
+++ 07_JDBC/src/fr/formation/database/MainApp.java
@@ -1,11 +1,12 @@
 package fr.formation.database;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import org.postgresql.*;
 
 public class MainApp
 {
@@ -13,19 +14,28 @@ public class MainApp
 	public static void main (String[] args)
 	{
 		Connection connection = null;
+		BufferedReader br = null;
 		
 		try
 		{
-			Class.forName ("org.postgresql.Driver");
-			connection = DriverManager.getConnection ("localhost:8080", "hugo", "pommeQ69");
-			Statement st = connection.createStatement ();
-			ResultSet rs = st.executeQuery ("SELECT * FROM table");
+			br = new BufferedReader (new InputStreamReader (System.in));
+			String pwd = br.readLine ();
 			
+			Class.forName ("org.postgresql.Driver");
+			connection = DriverManager.getConnection ("jdbc:postgresql://localhost:8080/garage", "hugo", pwd);
+			Statement st = connection.createStatement ();
+			ResultSet rs = st.executeQuery ("SELECT * FROM Voiture INNER JOIN Marque ON Voiture.id_marque = Marque.id;");
+			
+			int i = 0;
 			while (rs.next ())
 			{
-				String nom = rs.getString ("Nom");
+				String marque = rs.getString ("libelle");
+				String pays = rs.getString ("pays");
+				String couleur = rs.getString ("couleur");
 				String ref = rs.getString ("ref");
-				// ...
+				String nbPorte = rs.getString ("nb_porte");
+				String prix = rs.getString ("prix");
+				System.out.println (++i + " >  " + marque + "    " + pays + "    " + couleur + "    " + ref + "    " + nbPorte + "    " + prix);
 			}
 			
 			rs.close ();
