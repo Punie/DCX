ebedd23e01d817b85043ac77ba36417ad8dcc485
Hugo Saracino <hugo.saracino@gmail.com>
2016-11-03 17:21:02 +0100

    Switchin'

--- 08_HTML/05_BSExamples/blog/blog.js
+++ 08_HTML/05_BSExamples/blog/blog.js
@@ -6,21 +6,42 @@ var doLoad = function ()
   var comment = document.getElementById('comment');
   var comments = document.getElementById('comments');
   var sub = document.getElementById('sub');
+  var count = 0;
 
   sub.onclick = function ()
   {
-    comments.innerHTML +=
-      '<li class="list-group-item">' +
-        '<div class="media">' +
-          '<div class="media-body">' +
-            '<h4 class="media-heading">John Doe <small><i>Posted on February 19, 2016</i></small></h4>' +
-            '<p>' + comment.value + '</p>' +
+    count += 1;
+    if (count % 2 == 1)
+    {
+      comments.innerHTML +=
+        '<li class="list-group-item">' +
+          '<div class="media">' +
+            '<div class="media-body">' +
+              '<h4 class="media-heading">John Doe <small><i>Posted on February 19, 2016</i></small></h4>' +
+              '<p>' + comment.value + '</p>' +
+            '</div>' +
+            '<div class="media-left">' +
+              '<img src="img/img_avatar1.png" class="media-object" style="width:60px">' +
+            '</div>' +
           '</div>' +
-          '<div class="media-left">' +
-            '<img src="img/img_avatar1.png" class="media-object" style="width:60px">' +
+        '</li>';
+    }
+    else
+    {
+      comments.innerHTML +=
+        '<li class="list-group-item">' +
+          '<div class="media">' +
+            '<div class="media-body">' +
+              '<h4 class="media-heading">Jane Doe <small><i>Posted on February 19, 2016</i></small></h4>' +
+              '<p>' + comment.value + '</p>' +
+            '</div>' +
+            '<div class="media-left">' +
+              '<img src="img/img_avatar2.png" class="media-object" style="width:60px">' +
+            '</div>' +
           '</div>' +
-        '</div>' +
-      '</li>';
+        '</li>';
+    }
+
   };
 };
 
