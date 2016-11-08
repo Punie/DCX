19e1fe1656c5f74b29ad659d4ba6f12e2f2e4f39
Hugo Saracino <hugo.saracino@gmail.com>
2016-11-08 17:21:36 +0100

    Cookies

--- 10_Garage/src/fr/formation/servlet/DisplayGarageServlet.java
+++ 10_Garage/src/fr/formation/servlet/DisplayGarageServlet.java
@@ -21,49 +21,71 @@ public class DisplayGarageServlet extends HttpServlet
     public void doGet (HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException
     {
-        voitures = VoitureDAO.getAll ();
-        
-        response.setContentType("text/html");
-
-        PrintWriter out = response.getWriter();
-        
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<meta charset=\"utf-8\" />");
-        out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />");
-        out.println("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" />");
-        out.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js\"></script>");
-        out.println("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>"); 
-        out.println("<title>Garage</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<div class=\"container\">");
-        out.println ("<h1>Web Garage</h1>");
-        out.println("</div>");
-        out.println("<div class=\"container\">");
-        out.println ("<table class=\"table table-bordered table-condensed\">");
-        out.println ("<tr>" + "<td><strong>Id</strong></td>" + 
-                              "<td><strong>Ref</strong></td>" + 
-                              "<td><strong>Marque</strong></td>" + 
-                              "<td><strong>Pays</strong></td>" + 
-                              "<td><strong>Couleur</strong></td>" + 
-                              "<td><strong>Nb Portes</strong></td>" + 
-                              "<td><strong>Prix</strong></td>" + "</tr>");
-        for (Voiture v : voitures)
+        Cookie[] cookies = request.getCookies ();
+        if (cookies != null)
         {
-            out.println ("<tr>" + "<td>" + v.id () + "</td>" + 
-                                  "<td>" + v.ref () + "</td>" + 
-                                  "<td>" + v.marque ().libelle () + "</td>" + 
-                                  "<td>" + v.marque ().pays () + "</td>" +
-                                  "<td>" + v.couleur () + "</td>" + 
-                                  "<td>" + v.nbPorte () + "</td>" +
-                                  "<td>" + v.prix () + "</td>" + "</tr>");
+            for (int i = 0 ; i < cookies.length ; i++)
+            {
+                String name = cookies[i].getName ();
+                // String value = cookies[i].getValue ();
+                
+                if (name.equals ("login"))
+                {
+                    voitures = VoitureDAO.getAll ();
+                    
+                    response.setContentType("text/html");
+
+                    PrintWriter out = response.getWriter();
+                    
+                    out.println("<!DOCTYPE html>");
+                    out.println("<html>");
+                    out.println("<head>");
+                    out.println("<meta charset=\"utf-8\" />");
+                    out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />");
+                    out.println("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" />");
+                    out.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js\"></script>");
+                    out.println("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>"); 
+                    out.println("<title>Garage</title>");
+                    out.println("</head>");
+                    out.println("<body>");
+                    out.println("<div class=\"container\">");
+                    out.println ("<h1>Web Garage</h1>");
+                    out.println("</div>");
+                    out.println("<div class=\"container\">");
+                    out.println ("<table class=\"table table-bordered table-condensed\">");
+                    out.println ("<tr>" + "<td><strong>Id</strong></td>" + 
+                                          "<td><strong>Ref</strong></td>" + 
+                                          "<td><strong>Marque</strong></td>" + 
+                                          "<td><strong>Pays</strong></td>" + 
+                                          "<td><strong>Couleur</strong></td>" + 
+                                          "<td><strong>Nb Portes</strong></td>" + 
+                                          "<td><strong>Prix</strong></td>" + "</tr>");
+                    for (Voiture v : voitures)
+                    {
+                        out.println ("<tr>" + "<td>" + v.id () + "</td>" + 
+                                              "<td>" + v.ref () + "</td>" + 
+                                              "<td>" + v.marque ().libelle () + "</td>" + 
+                                              "<td>" + v.marque ().pays () + "</td>" +
+                                              "<td>" + v.couleur () + "</td>" + 
+                                              "<td>" + v.nbPorte () + "</td>" +
+                                              "<td>" + v.prix () + "</td>" + "</tr>");
+                    }
+                    out.println ("</table>");
+                    out.println("</div>");
+                    out.println("</body>");
+                    out.println("</html>");
+                    
+                    return;
+                }
+            }
+            response.sendRedirect ("login.html"); 
         }
-        out.println ("</table>");
-        out.println("</div>");
-        out.println("</body>");
-        out.println("</html>");
+        else
+        {
+            response.sendRedirect ("login.html");
+        }
+        
+        
     }
     
     public void destroy()
@@ -71,4 +93,5 @@ public class DisplayGarageServlet extends HttpServlet
         DBFactory.closeConnection ();
         System.out.println ("Servlet destroyed !");
     }
+
 }
